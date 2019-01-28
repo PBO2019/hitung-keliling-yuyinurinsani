d8758707a5d2f29cc6b97dfc8ea6e44cf807fa9d
yuyinurinsani <46981424+yuyinurinsani@users.noreply.github.com>
2019-01-28 19:24:39 +0700

    Add files via upload

---
+++ Kelilinglingkaran.java
@@ -0,0 +1,24 @@
+public class Kelilinglingkaran {
+   private double PI=3.14 ;
+   private double jari=5 ;
+   private double kll ;
+
+   public void hitungkeliling () {
+       this.kll = 2* PI *  jari ;
+   }
+
+   public void setPI (double PI){
+       this.PI = PI ;
+   }
+
+   public void setjari (double jari){
+       this.jari= jari;
+    }
+
+    public double getkll () {
+       return kll;
+    }
+    }
+
+
+
